6ab96f9052105a13599ab019f69daceee6007435
pascal Zhang <panamepoul@gmail.com>
2025-10-11 17:27:39 +0200

    add Subtennatility

--- frontend/src/components/imagesDisplay.tsx
+++ frontend/src/components/imagesDisplay.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import { useConversationStore } from '@/libs/zustand/conversation';
-import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
 const WIDTH = 320;
 const HEIGHT = 180;
-const defaultImage =
+export const defaultImage =
   'https://static.wikia.nocookie.net/virtualyoutuber/images/e/e5/Gremlin_Chibidoki_PNG.png/revision/latest?cb=20250311221031';
+
+const subtennatilityScore =() =>  {
+  return Math.floor(Math.random() * 100);
+};
 
 export const ImagesDisplay = () => {
   const { imagesStored } = useConversationStore();
@@ -23,18 +26,22 @@ export const ImagesDisplay = () => {
             src={`data:image/png;base64,${image.base64}`}
             width={WIDTH}
             height={HEIGHT}
+
             alt="image"
             onError={(e) => {
               e.currentTarget.src = defaultImage;
             }}
           />
           <p>{image.description}</p>
+          <div className="flex flex-row gap-2 items-center">
           <Button
             onClick={() => window.open('https://www.amazon.com/', '_blank')}
             variant="outline"
-          >
+            >
             buy it
           </Button>
+          <p>Subtennatility Score: {subtennatilityScore()} / 100</p>
+          </div>
         </div>
       ))}
     </div>
